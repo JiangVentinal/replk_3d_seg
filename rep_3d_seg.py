69260f8470c0a2caa4cf7656d7a2b030538b51b5
JiangVentinal <sjshangjiang@163.com>
2023-07-22 20:54:18 +0800

    encoder

---
+++ rep_3d_seg.py
@@ -0,0 +1,171 @@
+# -*- coding = utf-8 -*-
+# @Time:  10:07
+# @Author:JiangShang
+# @File：rep_3d_seg.py
+# @Software: PyCharm
+from rep_3d_encoder import create_RepLKNet31B
+
+from typing import Tuple
+
+import torch.nn as nn
+import torch
+from monai.networks.blocks.dynunet_block import UnetOutBlock
+from monai.networks.blocks.unetr_block import UnetrBasicBlock, UnetrUpBlock
+from typing import Union
+import torch.nn.functional as F
+
+
+
+class RepSeg31(nn.Module):
+    def __init__(self,
+                 in_chans=1,
+                 out_chans=13,
+                 depths=[2,2,18,2],
+                 feat_size=[128,256,512,1024],
+                 spatial_dims=3,
+                 norm_name: Union[Tuple, str] = "instance",
+                 res_block: bool = True,
+                 hidden_size: int = 2048,
+                 ):
+        super().__init__()
+
+        self.spatial_dims = spatial_dims
+        self.in_chans = in_chans
+        self.out_chans = out_chans
+        self.depths = depths
+        self.feat_size = feat_size
+        self.hidden_size = hidden_size
+
+        self.rep = create_RepLKNet31B(small_kernel_merged=False)
+
+        self.encoder1 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.in_chans,
+            out_channels=self.feat_size[0],
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.encoder2 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[0],
+            out_channels=self.feat_size[1],
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.encoder3 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[1],
+            out_channels=self.feat_size[2],
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.encoder4 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[2],
+            out_channels=self.feat_size[3],
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+
+        self.encoder5 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[3],
+            out_channels=self.hidden_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+
+        self.decoder5 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.hidden_size,
+            out_channels=self.feat_size[3],
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder4 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[3],
+            out_channels=self.feat_size[2],
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder3 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[2],
+            out_channels=self.feat_size[1],
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder2 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[1],
+            out_channels=self.feat_size[0],
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.decoder1 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=self.feat_size[0],
+            out_channels=self.feat_size[0],
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=res_block,
+        )
+        self.out = UnetOutBlock(spatial_dims=spatial_dims, in_channels=128, out_channels=self.out_chans)
+
+    def forward(self, x_in):
+        outs = self.rep(x_in)
+        # print(outs[0].size())
+        # print(outs[1].size())
+        # print(outs[2].size())
+        # print(outs[3].size())
+        enc1 = self.encoder1(x_in)
+        # print(enc1.size())
+        x2 = outs[0]
+        enc2 = self.encoder2(x2)
+        # print(enc2.size())
+        x3 = outs[1]
+        enc3 = self.encoder3(x3)
+        # print(enc3.size())
+        x4 = outs[2]
+        enc4 = self.encoder4(x4)
+        # print(enc4.size())
+        # dec4 = self.proj_feat(outs[3], self.hidden_size, self.feat_size)
+        enc_hidden = self.encoder5(outs[3])
+        dec3 = self.decoder5(enc_hidden, enc4)
+        dec2 = self.decoder4(dec3, enc3)
+        dec1 = self.decoder3(dec2, enc2)
+        dec0 = self.decoder2(dec1, enc1)
+        out = self.decoder1(dec0)
+
+        # feat = self.conv_proj(dec4)
+
+        return self.out(out)
+
+
+if __name__ == '__main__':
+    data = torch.randn(size=[1, 1, 96, 96, 96])
+    model = RepSeg31()
+    print(model(data).shape)
+
+
+
